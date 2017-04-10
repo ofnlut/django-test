68b97d2c171ddb477f2e4aef03000ae55ab279b0
ofnlut <alonzo.mozee@gmail.com>
2017-04-10 16:55:37 -0400

    fix settings

--- newsite/settings.py
+++ newsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sf_nf@g6&*+^vw7f+&*q+-8t@e4gaelciyaxf6x6813gvbu88s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
